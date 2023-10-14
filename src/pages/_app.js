3e33e223f3ed31d0ae06fc1aa52e42e4b05af059
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-10-14 13:40:56 -0700

    scss & create component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+// import "../styles/globals.scss";
+// import "../styles/reset.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
